95110f64ffbdb0026a56cf0848b47fea19cff67b
Calvin Chung <calvin@Calvins-MacBook-Pro.local>
2022-09-13 13:48:03 -0700

    added json server

--- src/api/api.js
+++ src/api/api.js
@@ -8,20 +8,13 @@ export default function apiRequest() {
   const api_key = process.env.REACT_APP_API_KEY;
   // app.use(cors());
   
-  axios.get(`http://api.sportradar.us/formula1/trial/v2/en/sport_events/sr:stage:686252/probabilities.json?api_key=http://api.sportradar.us/formula1/trial/v2/en/sport_events/sr:stage:686252/probabilities.json?api_key=${api_key}`)
+  // axios.get(`http://api.sportradar.us/formula1/trial/v2/en/sport_events/sr:stage:686252/probabilities.json?api_key=http://api.sportradar.us/formula1/trial/v2/en/sport_events/sr:stage:686252/probabilities.json?api_key=${api_key}`)
+  axios.get(`http://localhost:3001/info`)
     .then(data => {
       console.log(data);
     })
     .catch(error => {
       console.log(error);
     })
-
-  // axios.get(`https://official-joke-api.appspot.com/random_joke`)
-  //   .then(data => {
-  //     console.log(data.data);
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //   })
-
+    
 }
