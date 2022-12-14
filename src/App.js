5e7ec7d92a5089f4093a5603fc485437aba009b5
Calvin Chung <calvin-chung@live.com>
2022-12-13 16:25:34 -0800

    cleaning

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import './App.css';
 import SearchBar from './components/SearchBar';
 import TopNav from './components/NavBar.js'
 import RaceOdds from './components/RaceOdds'
-import useInfo from './api/useInfo';
 import Loading from './components/Loading';
 import Results from './components/Results';
 import { db } from '.';
-import { collection, doc, getDocs } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
 import SelectRace from './components/SelectRace';
 import { SeasonContext } from './contexts/SeasonContext';
 import { CurrentRaceContext } from './contexts/CurrentRaceContext';
@@ -52,23 +51,29 @@ function App() {
 
   return (
     <div className="App">
-      <BetContext.Provider value={[bet, setBet]}>
-        <DriverContext.Provider value={[driver, setDriver]}>
-          <SeasonContext.Provider value={[seasonRaces]}>
-            <BalanceContext.Provider value={[balance, setBalance]}>
-              <BetDriverContext.Provider value={[betDriver, setBetDriver]}>
-                <ResultsContext.Provider value={[results, setResults]}>
+      <BetContext.Provider value={[bet, setBet]}> {/* State for the selected Bet amount */}
+        <DriverContext.Provider value={[driver, setDriver]}> {/* State for all Drivers in a selected race */}
+          <SeasonContext.Provider value={[seasonRaces]}> {/* State for all the info in a Season via Firestore request - data, probabilities, results */}
+            <BalanceContext.Provider value={[balance, setBalance]}> {/* State for player funds */}
+              <BetDriverContext.Provider value={[betDriver, setBetDriver]}> {/* State for the selected Driver */}
+                <ResultsContext.Provider value={[results, setResults]}> {/* State for results of a selected race */}
 
                   <TopNav />
                   <main>
-                    <RenderResultsContext.Provider value={[render, setRender]}>
-                      <CurrentRaceContext.Provider value={[race, setRace]}>
+                    <RenderResultsContext.Provider value={[render, setRender]}> {/* State for rendering Results after placing bet */}
+                      <CurrentRaceContext.Provider value={[race, setRace]}> {/* State for selected Race */}
 
                         <div className="content-body">
                           <p className='race'>{race}</p>
                           {seasonRaces ? <SelectRace /> : <Loading />}
                           {seasonRaces && race ? <RaceOdds /> : <p></p>}
-                          {seasonRaces && race ? <SearchBar /> : <p className='message'>Welcome to Betting Formula! Please select a race to get started.</p>}
+                          {seasonRaces && race ? <SearchBar /> : 
+                          <p className='message'>Welcome to Betting Formula! Please select a race to get started. <br></br><br></br>
+                            How To Play: <br></br> 
+                            1. Select Race to bet on <br></br>
+                            2. Select Driver to bet on <br></br>
+                            3. Select Amount to bet
+                          </p>}
                           {render ? <Results /> : <p></p>}
                         </div>
 
